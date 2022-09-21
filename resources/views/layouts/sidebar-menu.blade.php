741b337a92266256f7d92399007366a47533bca9
Shubham Chhabra <shubhamchhabra222@gmail.com>
2022-09-21 10:46:43 +0530

    static pages strt

--- resources/views/layouts/sidebar-menu.blade.php
+++ resources/views/layouts/sidebar-menu.blade.php
@@ -2,7 +2,7 @@
     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
       <li class="nav-item">
         <router-link to="/dashboard" class="nav-link">
-          <i class="nav-icon fas fa-tachometer-alt blue"></i>
+          <i class="nav-icon fas fa-tachometer-alt white"></i>
           <p>
             Dashboard
           </p>
@@ -12,7 +12,7 @@
       @can('isAdmin')
         <li class="nav-item">
           <router-link to="/users" class="nav-link">
-            <i class="fa fa-users nav-icon blue"></i>
+            <i class="fa fa-users nav-icon white"></i>
             <p>Users</p>
           </router-link>
         </li>
@@ -23,7 +23,7 @@
       @can('isAdmin')
       <li class="nav-item has-treeview">
         <a href="#" class="nav-link">
-          <i class="nav-icon fas fa-cog green"></i>
+          <i class="nav-icon fas fa-cog white"></i>
           <p>
             Settings
             <i class="right fas fa-angle-left"></i>
@@ -33,7 +33,7 @@
 
           <li class="nav-item">
             <router-link to="/product/category" class="nav-link">
-              <i class="nav-icon fas fa-list-ol green"></i>
+              <i class="nav-icon fas fa-list-ol white"></i>
               <p>
                 Subjects
               </p>
@@ -41,7 +41,7 @@
           </li>
           <li class="nav-item">
             <router-link to="/product/tag" class="nav-link">
-              <i class="nav-icon fa-solid fa-book-open-reader green"></i>
+              <i class="nav-icon fas fa-file-alt white"></i>
               <p>
                 Services
               </p>
@@ -49,31 +49,40 @@
           </li>
           <li class="nav-item">
             <router-link to="/faq" class="nav-link">
-              <i class="nav-icon fas fa-list orange"></i>
+              <i class="nav-icon fas fa-question-circle white"></i>
               <p>
                 FAQ
               </p>
             </router-link>
           </li>
     
-    
-            <li class="nav-item">
+          <li class="nav-item">
+            <router-link to="/testimonial" class="nav-link">
+                <i class="nav-icon fas fa-star white"></i>
+                <p>
+                    Testimonial
+                </p>
+            </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/static-pages" class="nav-link">
+              <i class="nav-icon fas fa-paste white"></i>
+              <p>
+                  Static Pages
+              </p>
+          </router-link>
+      </li>
+
+            {{-- <li class="nav-item">
               <router-link to="/developer" class="nav-link">
                   <i class="nav-icon fas fa-cogs white"></i>
                   <p>
                       Developer
                   </p>
               </router-link>
-            </li>
+            </li> --}}
         </ul>
-            <li class="nav-item">
-                <router-link to="/testimonial" class="nav-link">
-                    <i class="nav-icon fas fa-list yellow"></i>
-                    <p>
-                        Testimonial
-                    </p>
-                </router-link>
-            </li>
+           
       </li>
 
       @endcan
